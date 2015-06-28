f7912a0161dcb534942bea97631ab13859300d84
agx540 <alexander.geltermair@live.com>
2015-06-28 12:39:39 +0200

    Logstash: Mutate and Input date and float to EL

--- ELK/2Step.bat
+++ ELK/2Step.bat
@@ -1,3 +1,7 @@
+:: clear log files from logstash log directory
+del C:\Users\Alexander\Desktop\TestLogs\NAS201_eurotech_blau\streams_logs\_Logs\copy\EuroTech201Statistics_20150607-000000.csv
+
+
 :: clear old elasticsearch index
 node CleanOldIndexes.js
 
